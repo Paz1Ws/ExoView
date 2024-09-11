5e51908baa7b53efc6e86155a6515934e4435e24
rewardmnx <rewardmnx@gmail.com>
2024-09-11 17:54:27 +0000

    test commit

---
+++ lib/config/theme/colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color veryLightGray = Color(0xFFF2F2F2);
+  static const Color lightGray = Color(0xFFCCCCCC);
+  static const Color darkGray = Color(0xFF676567);
+  static const Color veryDarkGray = Color(0xFF333333);
+  static const Color brightTealGreen = Color(0xFF0BE9A6);
+  static const Color softPurple = Color(0xFFB5A2CA);
+  static const Color mediumPurple = Color(0xFF9883AE);
+  static const Color purple = Color.fromARGB(255, 88, 50, 126);
+  static const Color veryDarkPurple = Color(0xFF321E48);
+}
+
